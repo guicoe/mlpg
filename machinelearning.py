350849943d2dda5e5ee4428bee987e2a5bed45f4
guicoe <ernest.guico@gmail.com>
2023-10-07 01:20:05 -0400

    Initial working implementation (#1)
    * Initial implementation of machinelearning module
    
    * Seemingly working implementation
    
    * Redesign ml module
    
    * Add IInitializer interface along with common implementations
    
    * Add docstrings to classes
    
    * Create unit tests for machinelearning module
    
    * Fix linear synapse backprop output
    
    * Reformat docstrings in accordance to PEP 257
    
    * Move logical conjunctions to improve readability
    
    * Initialize biases using same initializer used for weights

---
+++ machinelearning.py
@@ -0,0 +1,203 @@
+import abc
+import numpy as np
+
+
+class IInitializer(metaclass=abc.ABCMeta):
+    """
+    Interface for matrix initializers
+    """
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'initialize')
+                and callable(subclass.initialize)
+                or NotImplemented)
+
+    @abc.abstractmethod
+    def initialize(self, size):
+        raise NotImplementedError
+
+
+class Normal(IInitializer):
+    """
+    Implemnentation of IInitializer interface using normal distribution
+    """
+    def __init__(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
+
+    def initialize(self, size):
+        rng = np.random.default_rng()
+        return rng.normal(self.mu, self.sigma, size=size)
+
+
+class Uniform(IInitializer):
+    """
+    Implementation of IInitializer interface using uniform distribution
+    """
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+
+    def initialize(self, size):
+        rng = np.random.default_rng()
+        return rng.uniform(self.min, self.max, size=size)
+
+        
+class ISynapse(metaclass=abc.ABCMeta):
+    """
+    Interface for neural network components
+    """
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'feedforward')
+                and callable(subclass.feedforward)
+                and hasattr(subclass, 'backprop')
+                and callable(subclass.backprop)
+                or NotImplemented)
+
+    @abc.abstractmethod
+    def feedforward(self, x):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def backprop(self, dy):
+        raise NotImplementedError
+
+
+class LinearSynapse(ISynapse):
+    """
+    Main neural network synapse which uses weights and biases
+    """
+    def __init__(self, weights, biases, stepSize):
+        self.weights = weights
+        self.biases = biases
+        self.stepSize = stepSize
+
+    def feedforward(self, x):
+        self.x = x
+        return self.weights@self.x + self.biases
+
+    def backprop(self, dy):
+        dWeights = dy@self.x.transpose()
+        result = self.weights.transpose()@dy
+        self.update(dWeights, dy)
+        return result
+
+    def update(self, dWeights, dBiases):
+        self.weights -= self.stepSize*dWeights
+        self.biases -= self.stepSize*dBiases
+
+
+class IActivation(ISynapse):
+    """
+    Interface for activation function synapses
+    """
+    pass
+
+
+class Sigmoid(IActivation):
+    """
+    Non-linear synapse for neural network
+    """
+    def feedforward(self, x):
+        self.z = 1 / (1 + np.exp(-x))
+        return self.z
+
+    def backprop(self, dy):
+        dz = np.multiply(self.z, 1 - self.z)
+        return np.multiply(dz, dy)
+
+
+class Identity(IActivation):
+    """
+    Trivial activation function
+    """
+    def feedforward(self, x):
+        return x
+
+    def backprop(self, dy):
+        return dy
+
+
+class ICostFunction(metaclass=abc.ABCMeta):
+    """
+    Interface for neural network cost function
+    """
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'cost')
+                and callable(subclass.cost)
+                and hasattr(subclass, 'derivative')
+                and callable(subclass.derivative)
+                or NotImplemented)
+
+    @abc.abstractmethod
+    def cost(self, actual, expected):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def derivative(self, actual, expected):
+        raise NotImplementedError
+
+
+class SquaredError(ICostFunction):
+    """
+    Squared error implementation of ICostFunction interface
+    """
+    def cost(self, actual, expected):
+        return 0.5*np.sum((actual - expected)**2)
+
+    def derivative(self, actual, expected):
+        return actual - expected
+
+
+class NeuralNet:
+    """
+    Class for building, training, and using neural network models
+    """
+    def __init__(
+            self,
+            neuralLayers: list[int],
+            activations: list[IActivation],
+            initializer: IInitializer,
+            cost: ICostFunction,
+            stepSize: float):
+        self.build_synapses(neuralLayers, activations, initializer, stepSize)
+        self.cost = cost
+
+    def build_synapses(
+            self,
+            neuralLayers: list[int],
+            activations: list[IActivation],
+            initializer: IInitializer,
+            stepSize: float):
+        self.synapses = []
+        for n, m in zip(neuralLayers, neuralLayers[1:]):
+            weights = initializer.initialize((m, n))
+            biases = initializer.initialize((m, 1))
+            self.synapses.append(LinearSynapse(weights, biases, stepSize))
+            if activations:
+                self.synapses.append(activations.pop(0))
+
+    def cycle(self, x, y):
+        self.feedforward(x)
+        self.backprop(y)
+
+    def train_epoch(self, training_set):
+        for x, y in training_set:
+            self.cycle(x, y)
+
+    def train(self, training_set, epochs):
+        for _ in range(epochs):
+            self.train_epoch(training_set)
+
+    def feedforward(self, x):
+        for s in self.synapses:
+            x = s.feedforward(x)
+        self.output = x
+        return self.output
+
+    def backprop(self, expected):
+        dy = self.cost.derivative(self.output, expected)
+        for s in reversed(self.synapses):
+            dy = s.backprop(dy)
--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import machinelearning as ml
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def main():
-    pass
+    nl = [1, 5, 1]
+    sig = ml.Sigmoid()
+    uniform = ml.Normal(0, 2)
+    cost = ml.SquaredError()
+    lr = 0.1
+    n = 200
+    epochs = 500
+    nn = ml.NeuralNet(nl, [sig], uniform, cost, lr)
+    rng = np.random.default_rng()
+    xs = rng.uniform(0, 2*np.pi, n)
+    training_set = [(np.array([[x]]), np.array([[np.sin(x)]])) for x in xs]
+    nn.train(training_set, epochs)
+    tnum = 100
+    xtest = np.linspace(0, 2*np.pi, tnum)
+    ytest = np.array([nn.feedforward(np.array([[x]]))[0] for x in xtest])
+    yp = np.sin(xtest)
+    plt.plot(xtest, yp, "r", label="y=f(x)")
+    plt.scatter(xtest, ytest, marker=".", label="Model")
+    plt.legend()
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
